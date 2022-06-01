d2dd4d0744d12d4043e151ddbbc22de988d7aef5
Simon Zhang <zhanghan.simon@gmail.com>
2022-06-01 14:02:04 -0400

    v1.1.25

--- gear/cli.py
+++ gear/cli.py
@@ -162,7 +162,6 @@ def meter_block_convert_to_eth_block(block):
     block['difficulty'] = '0x0'
     block['extraData'] = '0x'
     block['baseFeePerGas'] = '0x0'
-    block['mixHash'] = '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421'
     if 'kblockData' in block:
         del block['kblockData']
     if 'powBlocks' in block:
